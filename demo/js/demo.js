ea00908c099a5e51747c3c46ae4b384050151552
dagnelies <arnaud.dagnelies@gmail.com>
2022-09-27 17:50:16 +0200

    demos and stuff

---
+++ demo/js/demo.js
@@ -0,0 +1,49 @@
+import * as webauthn from '/dist/webauthn.min.js'
+
+ const app = new Vue({
+    el: '#app',
+    data: {
+      username: null,
+      isRegistered: false,
+      isAuthenticated: false,
+      isExternal: false
+    },
+    methods: {
+      async checkIsRegistered() {
+        console.log(this.username + ' => ' + !!window.localStorage.getItem(this.username))
+        this.isRegistered = !!window.localStorage.getItem(this.username)
+      },
+      async register() {
+        let res = await webauthn.register(this.username, window.crypto.randomUUID(),{authType: this.isExternal ? 'extern' : 'auto'})
+        this.$buefy.toast.open({
+            message: 'Registered!',
+            type: 'is-success'
+        })
+
+        console.log(res)
+
+        this.isAuthenticated = true;
+        window.localStorage.setItem(this.username, res.credential.id)
+        await this.checkIsRegistered()
+      },
+      async login() {
+        let credentialId = window.localStorage.getItem(this.username)
+        let res = await webauthn.login([credentialId], window.crypto.randomUUID(), {isExternal: this.isExternal})
+        console.log(res)
+
+        this.isAuthenticated = true;
+        this.$buefy.toast.open({
+            message: 'Signed in!',
+            type: 'is-success'
+        })
+      },
+      async logout() {
+        this.isAuthenticated = false;
+        this.$buefy.toast.open({
+            message: 'Signed out!',
+            type: 'is-success'
+        })
+
+      }
+    }
+ })
