adaeb1e6605588196708fdc89bd07a341e7432aa
Arnaud Dagnelies <arnaud.dagnelies@gmail.com>
2022-11-14 09:41:52 +0100

    added /dist/types to .gitignore

--- dist/types/authenticators.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function parseAuthBuffer(authData: ArrayBuffer): any;
-export declare function extractAaguid(authData: ArrayBuffer): string;
-export declare function resolveAuthenticatorName(aaguid: string): string;
-export declare function updateDevicesMetadata(): Promise<void>;
