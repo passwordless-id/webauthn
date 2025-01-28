cdb4adc6482aca07f57885e47b58f089305b3db1
Paul Taiwo <ayopaulot@gmail.com>
2025-01-28 01:44:37 +0100

    chore: revert code move from the server to utils.

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,7 @@
 import { parsers } from "./index";
 import { parseAuthenticator, parseClient, toAuthenticationInfo } from "./parsers";
-import { AuthenticationJSON, RegistrationJSON, RegistrationInfo, AuthenticationInfo, Base64URLString, CollectedClientData, UserInfo, CredentialInfo, AuthenticatorInfo, AuthenticatorParsed } from "./types";
+import { AuthenticationJSON, NamedAlgo, RegistrationJSON, RegistrationInfo, AuthenticationInfo, Base64URLString, CollectedClientData, UserInfo, CredentialInfo, AuthenticatorInfo, AuthenticatorParsed } from "./types";
 import * as utils from './utils'
-import { verifySignature } from "./utils";
 
 
 export function randomChallenge() {
@@ -117,4 +116,107 @@ export async function verifyAuthentication(authenticationJson: AuthenticationJSO
         throw new Error(`Unexpected authenticator counter: ${authenticator.signCount} (should be > ${expected.counter})`)
 
     return toAuthenticationInfo(authenticationJson, authenticator)
+}
+
+
+// https://w3c.github.io/webauthn/#sctn-public-key-easy
+// https://www.iana.org/assignments/cose/cose.xhtml#algorithms
+/*
+User agents MUST be able to return a non-null value for getPublicKey() when the credential public key has a COSEAlgorithmIdentifier value of:
+
+-7 (ES256), where kty is 2 (with uncompressed points) and crv is 1 (P-256).
+
+-257 (RS256).
+
+-8 (EdDSA), where crv is 6 (Ed25519).
+*/
+function getAlgoParams(algorithm: NamedAlgo): AlgoParams {
+    switch (algorithm) {
+        case 'RS256':
+            return {
+                name: 'RSASSA-PKCS1-v1_5',
+                hash: 'SHA-256'
+            };
+        case 'ES256':
+            return {
+                name: 'ECDSA',
+                namedCurve: 'P-256',
+                hash: 'SHA-256',
+            };
+        // case 'EdDSA': Not supported by browsers
+        default:
+            throw new Error(`Unknown or unsupported crypto algorithm: ${algorithm}. Only 'RS256' and 'ES256' are supported.`)
+    }
+}
+
+type AlgoParams = RsaPssParams | EcKeyImportParams | EcdsaParams
+
+export async function parseCryptoKey(algorithm: NamedAlgo, publicKey: string): Promise<CryptoKey> {
+    const algoParams = getAlgoParams(algorithm)
+    const buffer = utils.parseBase64url(publicKey)
+    return crypto.subtle.importKey('spki', buffer, algoParams, false, ['verify'])
+}
+
+
+
+type VerifyParams = {
+    algorithm: NamedAlgo,
+    publicKey: Base64URLString,
+    authenticatorData: Base64URLString,
+    clientData: Base64URLString,
+    signature: Base64URLString,
+    verbose?: boolean, // Enables debug logs containing sensitive data like crypto keys
+}
+
+
+// https://w3c.github.io/webauthn/#sctn-verifying-assertion
+// https://w3c.github.io/webauthn/#sctn-signature-attestation-types
+/* Emphasis mine:
+
+6.5.6. Signature Formats for Packed Attestation, FIDO U2F Attestation, and **Assertion Signatures**
+
+[...] For COSEAlgorithmIdentifier -7 (ES256) [...] the sig value MUST be encoded as an ASN.1 [...]
+[...] For COSEAlgorithmIdentifier -257 (RS256) [...] The signature is not ASN.1 wrapped.
+[...] For COSEAlgorithmIdentifier -37 (PS256) [...] The signature is not ASN.1 wrapped.
+*/
+// see also https://gist.github.com/philholden/50120652bfe0498958fd5926694ba354
+export async function verifySignature({ algorithm, publicKey, authenticatorData, clientData, signature, verbose }: VerifyParams): Promise<boolean> {
+    let cryptoKey = await parseCryptoKey(algorithm, publicKey)
+
+    if(verbose) {
+        console.debug(cryptoKey)
+    }
+
+    let clientHash = await utils.sha256(utils.parseBase64url(clientData));
+
+    // during "login", the authenticatorData is exactly 37 bytes
+    let comboBuffer = utils.concatenateBuffers(utils.parseBase64url(authenticatorData), clientHash)
+
+    if(verbose) {
+        console.debug('Algorithm: ' + algorithm)
+        console.debug('Public key: ' + publicKey)
+        console.debug('Data: ' + utils.toBase64url(comboBuffer))
+        console.debug('Signature: ' + signature)
+    }
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/verify
+    let signatureBuffer = utils.parseBase64url(signature)
+    if(algorithm == 'ES256')
+        signatureBuffer = convertASN1toRaw(signatureBuffer)
+
+    const algoParams = getAlgoParams(algorithm)
+    const isValid = await crypto.subtle.verify(algoParams, cryptoKey, signatureBuffer, comboBuffer)
+
+    return isValid
+}
+
+function convertASN1toRaw(signatureBuffer :ArrayBuffer) {
+    // Convert signature from ASN.1 sequence to "raw" format
+    const signature = new Uint8Array(signatureBuffer);
+    const rStart = signature[4] === 0 ? 5 : 4;
+    const rEnd = rStart + 32;
+    const sStart = signature[rEnd + 2] === 0 ? rEnd + 3 : rEnd + 2;
+    const r = signature.slice(rStart, rEnd);
+    const s = signature.slice(sStart);
+    return new Uint8Array([...r, ...s]);
 }
